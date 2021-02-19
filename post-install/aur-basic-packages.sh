ce0850fbf6392d5c0166b4422a82d15df9b304e2
zaktorius <maximilianodanielleiva@gmail.com>
2021-02-19 19:27:31 -0300

    added no confirmation to install dependencies on aur packages

--- post-install/aur-basic-packages.sh
+++ post-install/aur-basic-packages.sh
@@ -3,13 +3,13 @@ mkdir /home/build
 
 # VSCODIUM AND MARKETPLACE
 cd /home/build/
-git clone https://aur.archlinux.org/vscodium-bin-marketplace && cd /home/build/vscodium-bin-marketplace && makepkg -si
+git clone https://aur.archlinux.org/vscodium-bin-marketplace && cd /home/build/vscodium-bin-marketplace && makepkg -si --noconfirm
 # Typora
 cd /home/build/
-git clone https://aur.archlinux.org/typora && cd /home/build/typora && makepkg -si
+git clone https://aur.archlinux.org/typora && cd /home/build/typora && makepkg -si --noconfirm
 # Pamac
 cd /home/build/
-git clone https://aur.archlinux.org/pamac-aur && cd /home/build/pamac-aur && makepkg -si
+git clone https://aur.archlinux.org/pamac-aur && cd /home/build/pamac-aur && makepkg -si --noconfirm
 
 # Clean dirs
 rm -rf /home/build
