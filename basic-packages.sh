6e45e290103a927719879e13654077f0126c955d
zaktorius <maximilianodanielleiva@gmail.com>
2021-02-18 21:25:45 -0300

    added basic packages

---
+++ basic-packages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pacstrap /mnt iproute2 man-db man-pages texinfo grub efibootmgr networkmanager iwd zsh git neovim vim-ale fzf the_silver_searcher s-tui xorg-server xorg-apps xdg-user-dirs htop sudo fakeroot nodejs npm yarn jdk11-openjdk firefox chromium filelight neofetch
