83fc3c443c010c8f6a2fac55e519256e070bdeda
zaktorius <maximilianodanielleiva@gmail.com>
2021-02-19 20:16:35 -0300

    modified aur package install dir

--- chroot/install.sh
+++ chroot/install.sh
@@ -2,17 +2,15 @@
 # Timezone setup
 ln -sf /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
 hwclock --systohc
-# AUR WARMUP
-cp ./sudoers-aur /etc/sudoers
+# locales conf
+cp ./locale.gen /etc/locale.gen
+locale-gen
+cp ./locale.conf /etc/locale.conf
 # HOSTNAME AND HOSTS
 cp ./hostname /etc/hostname
 cp ./hosts /etc/hosts
-# AUR BASIC INSTALL
-./aur-basic-packages.sh
 # ZAKTORIUS CREATION
 ./create-user.sh
-# RESTORE SUDO
-cp ./sudoers-default /etc/sudoers
 # Grub INstall
 grub-install --target=x86_64-efi
 grub-mkconfig -o /boot/grub/grub.cfg
--- post-install/aur-basic-packages.sh
+++ post-install/aur-basic-packages.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-mkdir /home/build
+mkdir ~/build
 
 # VSCODIUM AND MARKETPLACE
-cd /home/build/
-git clone https://aur.archlinux.org/vscodium-bin-marketplace && cd /home/build/vscodium-bin-marketplace && makepkg -si --noconfirm
+cd ~/build/
+git clone https://aur.archlinux.org/vscodium-bin-marketplace && cd vscodium-bin-marketplace && makepkg -si --noconfirm
 # Typora
-cd /home/build/
-git clone https://aur.archlinux.org/typora && cd /home/build/typora && makepkg -si --noconfirm
+cd ~/build/
+git clone https://aur.archlinux.org/typora && cd typora && makepkg -si --noconfirm
 # Pamac
-cd /home/build/
-git clone https://aur.archlinux.org/pamac-aur && cd /home/build/pamac-aur && makepkg -si --noconfirm
+cd ~/build/
+git clone https://aur.archlinux.org/pamac-aur && cd pamac-aur && makepkg -si --noconfirm
 
 # Clean dirs
 rm -rf /home/build
