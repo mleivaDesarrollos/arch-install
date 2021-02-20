5bd7d982b72d0616c516ee1be8d6c21c2d540866
Maximiliano Leiva <maximilianodanielleiva@gmail.com>
2021-02-20 16:48:46 -0300

    added some changes to chroot interactions

--- chroot/vivobook.sh
+++ chroot/vivobook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/chroot
+cd /home/arch-install/chroot
 ./default.sh
 # Copiando el archivo hosts y hostname de vivobook a la carpeta correspondiente de etc
 echo 'Copiando los hosts de vivobook laptop'
@@ -7,5 +7,9 @@ cp ./vivobook/hostname /etc/hostname
 cp ./vivobook/hosts /etc/hosts
 echo 'Habilitando servicio SDDM de KDE';
 ./enable-services-kde.sh
+echo 'entregando permisos sobre carpeta archinstall a zaktorius y moviendola dentro'
+cd /home
+mv /home/arch-install /home/zaktorius
+chown zaktorius:zaktorius /home/zaktorius/arch-install
 echo 'Saliendo de chroot'
 exit
