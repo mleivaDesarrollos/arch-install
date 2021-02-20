3fd57a0f18a4232055440c9a79c313138f5cfc00
zaktorius <maximilianodanielleiva@gmail.com>
2021-02-19 21:59:12 -0300

    added vivobook oc control service

--- post-install-aur/vivobook.sh
+++ post-install-aur/vivobook.sh
@@ -2,9 +2,18 @@
 # temp dir
 mkdir ~/build
 
+# Copying all oc vivobook controls
+sudo cp ../custom-services/vivobook-cpu-tdp-adjust.service /etc/systemd/system
+sudo cp ../helper-scripts/vivobook/low-tdp-ryzen /usr/bin
+sudo chmod +x /usr/bin/low-tdp-ryzen
+
 # Ryzenadj
 cd ~/build
 git clone https://aur.archlinux.org/ryzenadj-git && cd ryzenadj-git && makepkg -si --noconfirm
+
+# enable oc services
+sudo systemctl enable vivobook-cpu-tdp-adjust.service
+sudo systemctl start vivobook-cpu-tdp-adjust.service
 
 # clean temp dir
 rm -rf ~/build
