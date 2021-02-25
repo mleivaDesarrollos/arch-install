b2a86ea1d3a2ed28cf242c1a4fb66647502ab3d7
Maximiliano Leiva <maximilianodanielleiva@gmail.com>
2021-02-24 21:15:01 -0300

    added to basic installation libreoffice, inkscape, idea, and gimp

--- core-packages/basic-packages.sh
+++ core-packages/basic-packages.sh
@@ -4,4 +4,4 @@ pacstrap /mnt base linux linux-firmware
 echo 'Generamos el archivo FSTAB'
 genfstab -U /mnt >> /mnt/etc/fstab
 echo 'Instalamos los paquetes core'
-pacstrap /mnt iproute2 man-db man-pages texinfo grub efibootmgr networkmanager iwd zsh git neovim vim-ale fzf the_silver_searcher s-tui xdg-user-dirs htop sudo fakeroot nodejs npm yarn jdk11-openjdk firefox chromium filelight neofetch base-devel telegram-desktop mariadb thunderbird qbittorrent bluez bluez-utils ark openssh util-linux ufw
+pacstrap /mnt iproute2 man-db man-pages texinfo grub efibootmgr networkmanager iwd zsh git neovim vim-ale fzf the_silver_searcher s-tui xdg-user-dirs htop sudo fakeroot nodejs npm yarn jdk11-openjdk firefox chromium filelight neofetch base-devel telegram-desktop mariadb thunderbird qbittorrent bluez bluez-utils ark openssh util-linux ufw intellij-idea-community-edition libreoffice-still inkscape gimp
