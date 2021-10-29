0777933b74399ab99143de6428486d05aea4e885
Maximiliano Leiva <zaktorius@dellxps-max-arch.localdomain>
2021-10-29 19:13:41 -0300

    added handbrake and kdenlive to basic core packages

--- core-packages/basic-packages.sh
+++ core-packages/basic-packages.sh
@@ -4,4 +4,4 @@ pacstrap /mnt base linux linux-firmware
 echo 'Generamos el archivo FSTAB'
 genfstab -U /mnt >> /mnt/etc/fstab
 echo 'Instalamos los paquetes core'
-pacstrap /mnt iproute2 man-db man-pages texinfo grub efibootmgr networkmanager iwd zsh git neovim vim-ale fzf the_silver_searcher s-tui xdg-user-dirs htop sudo fakeroot nodejs npm yarn jdk11-openjdk firefox filelight neofetch base-devel telegram-desktop thunderbird qbittorrent bluez bluez-utils ark openssh util-linux ufw libreoffice-still inkscape gimp docker pipewire pipewire-media-session pipewire-pulse ntfs-3g
+pacstrap /mnt iproute2 man-db man-pages texinfo grub efibootmgr networkmanager iwd zsh git neovim vim-ale fzf the_silver_searcher s-tui xdg-user-dirs htop sudo fakeroot nodejs npm yarn jdk11-openjdk firefox filelight neofetch base-devel telegram-desktop thunderbird qbittorrent bluez bluez-utils ark openssh util-linux ufw libreoffice-still inkscape gimp docker pipewire pipewire-media-session pipewire-pulse ntfs-3g kdenlive handbrake
